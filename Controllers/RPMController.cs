f82f5ec69e945bd214fda31950a5ab7c8b996c96
Richard Murray <richard.murray@2commit.be>
2016-06-16 09:30:46 +0200

    First checkin

---
+++ Controllers/RPMController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RPMTest.Controllers
+{
+    public class RPMController : Controller
+    {
+        // GET: RPM
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
